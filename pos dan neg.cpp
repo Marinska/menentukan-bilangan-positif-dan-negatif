33b92ca716a32bff082b49184c3d14b339a210c6
Marinska <Marinska354@users.noreply.github.com>
2018-12-17 14:46:29 +0700

    content

---
+++ pos dan neg.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("Masukan nilai bilangan : ");
+	scanf("%d",&a);
+	if(a>0){
+		printf("Bilangan positif");
+	}else{
+		printf("Bilangan negatif");
+	}
+}
